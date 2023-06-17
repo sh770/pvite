973a038bbf420bb05aa77f3ef8509ea85188db5a
[shalom harpaz] <[sh770h@gmail.com]>
2023-06-18 00:28:12 +0300

    18/06

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pvite/", //This is the base url of your website
 })
